b08cccfae04258305dfed24e5bfd594f178936a2
Gustavo <gustavombelfort@gmail.com>
2020-07-27 13:18:14 -0300

    changes ingress/egress rules and added description to security group

--- security-groups.tf
+++ security-groups.tf
@@ -5,31 +5,8 @@ resource "aws_security_group" "ethereum" {
   vpc_id      = module.vpc.id
 
   ingress {
-    from_port   = "0"
-    to_port     = "0"
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    self        = true
-  }
-
-  ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = "0"
-    to_port     = "0"
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    self        = true
-  }
-
-  ingress {
-    from_port   = 8545
-    to_port     = 8545
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -42,17 +19,25 @@ resource "aws_security_group" "ethereum" {
   }
 
   ingress {
-    from_port   = 30303
-    to_port     = 30303
+    from_port   = 1337
+    to_port     = 1337
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+  ingress {
+    from_port   = 30303
+    to_port     = 30303
+    protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
   }
 
   tags = {
